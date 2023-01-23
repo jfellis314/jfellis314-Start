61e331d3d12ec72835ec2f69fac3c39fef9567e8
J Ellis <thelotris3.14tastic@gmail.com>
2023-01-23 16:06:16 +0000

    Completed Lab Boolean Operators

--- src/main/java/GuideMain.java
+++ src/main/java/GuideMain.java
@@ -24,7 +24,7 @@ public class GuideMain {
     public static void main(String[] args) {
 //      The Guide object constructor is written to take in an int parameter, which in this case is 1.
         Guide myGuide = new Guide(1);
-        String result = myGuide.guideMethod("hello from main!");
+        String result = myGuide.guideMethod("hello, from main!");
         System.out.println(result);
 
     }
